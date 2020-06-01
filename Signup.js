bcdef69a947bee8b355d2fa628934c51f07e623f
Nassiwa Veronica <veronicanassiwa@gmail.com>
2020-06-01 16:27:16 +0400

    signup changes

--- Signup.js
+++ Signup.js
@@ -1,22 +1,27 @@
-function sign(){
-    let email_address=document.getElementById("EmailAdd").value;
-    let first_name=document.getElementById("FistName").value;
-    let last_name=document.getElementById("LastName").value;
-    let password=document.getElementById("Passwordd").value;
-    
-  fetch('https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signup',{
-    method:'POST',
-    mode:"no-cors",
-    headers: {
-      'Content-type': 'application/json'
-  },
-    body: JSON.stringify({ 
-      email_address: "EmailAdd",
-      first_name: "FistName", 
-     last_name: "LastName", 
-      password: "Passwordd"})
-})
-  .then((res=>{console.log(res)}))
-  .catch((err=>{console.log(err)}))
+    function sign() {
+        let mail_address = document.getElementById("EmailAdd");
+        let fist_name = document.getElementById("FistName");
+        let lat_name = document.getElementById("LastName");
+        let pasword = document.getElementById("Passwordd");
+        const url = 'https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signup';
 
-}
+        fetch(url, {
+            method: "POST",
+            //mode: "no-cors",
+            headers: {
+                "Content - type ": "application/json",
+            },
+            body: JSON.stringify({
+                    "email_address": mail_address.value,
+                    "first_name": fist_name.value,
+                    "last_name": lat_name.value,
+                    "password": pasword.value
+                })
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) =>
+                    console.log(data))
+        })
+
+    }
--- login.js
+++ login.js
@@ -3,11 +3,17 @@ function lgin(){
     let password=document.getElementById('password1').value;
     fetch('https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signin',{
     method:'POST',
+    mode:"no-cors",
+    headers: {
+      'Content-type': 'application/json'
+  },
       body: JSON.stringify({ 
         email_address: "EmailAdd", 
         password: "Passwordd"})
 })
-    .then((response =>{console.log(response)
-    }))
-    .catch((err=>{console.log(err)}))
+.then((response)=>{ console.log(response)})
+.then((data)=>{
+  console.log(data);
+})
+.catch((err)=>{console.log(err)})
 }
