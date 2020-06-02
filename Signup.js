130c6f5f694de69f807e91a4958a1437b6628fe6
Nassiwa Veronica <veronicanassiwa@gmail.com>
2020-06-02 23:39:06 +0400

    signup changes

--- Signup.js
+++ Signup.js
@@ -1,27 +1,48 @@
-    function sign() {
-        let mail_address = document.getElementById("EmailAdd");
-        let fist_name = document.getElementById("FistName");
-        let lat_name = document.getElementById("LastName");
-        let pasword = document.getElementById("Passwordd");
-        const url = 'https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signup';
+function sign() {
 
-        fetch(url, {
+    let email_address = document.getElementById("EmailAdd");
+    let first_name = document.getElementById("FistName");
+    let last_name = document.getElementById("LastName");
+    let password = document.getElementById("Passwordd");
+
+    const url = "https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signup";
+    fetch(url, {
             method: "POST",
-            //mode: "no-cors",
             headers: {
-                "Content - type ": "application/json",
+                "Content-type": "application/json"
             },
             body: JSON.stringify({
-                    "email_address": mail_address.value,
-                    "first_name": fist_name.value,
-                    "last_name": lat_name.value,
-                    "password": pasword.value
-                })
-                .then((response) => {
-                    return response.json()
-                })
-                .then((data) =>
-                    console.log(data))
-        })
+                "email_address": email_address.value,
+                "first_name": first_name.value,
+                "last_name": last_name.value,
+                "password": password.value
+            })
+        }).then(response => response.json())
+        .then(data => {
 
-    }
+            if (data.status === 201) {
+                document.write("You have signed in successfully");
+
+            } else if (data.status === 405) {
+                document.write("Incorrect Details");
+            }
+
+        });
+
+}
+
+
+// fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-type ": "application/json",
+//         },
+//         body: JSON.stringify({
+//             "email_address": email_address.value,
+//             "first_name": first_name.value,
+//             "last_name": last_name.value,
+//             "password": password.value,
+//         })
+
+//     }).then(response => response.json())
+//     .then(data => console.log(data))
--- login.js
+++ login.js
@@ -1,19 +1,31 @@
-function lgin(){
-    let email_address=document.getElementById('LNemail').value;
-    let password=document.getElementById('password1').value;
-    fetch('https://epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signin',{
-    method:'POST',
-    mode:"no-cors",
-    headers: {
-      'Content-type': 'application/json'
-  },
-      body: JSON.stringify({ 
-        email_address: "EmailAdd", 
-        password: "Passwordd"})
-})
-.then((response)=>{ console.log(response)})
-.then((data)=>{
-  console.log(data);
-})
-.catch((err)=>{console.log(err)})
+function lgin() {
+
+    let email_address = document.getElementById("LNemail ");
+    let password = document.getElementById("password1");
+
+    const url = "https: //epicmail-sentongo-v2.herokuapp.com/api/v2/auth/signin";
+
+    fetch(url, {
+            method: "POST ",
+            //mode: "no-cors",
+            headers: {
+                "Content - type ": "application / json "
+            },
+            body: JSON.stringify({
+                "email_address ": email_address.value,
+                "password ": password.value,
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+
+            if (data.status === 201) {
+                document.getElementById("postig").innerHTML = "You have succesfully signed in";
+                document.write("You have signed in successfully");
+
+            } else if (data.status === 405) {
+                document.write("Incorrect Details");
+            }
+        });
+
 }
